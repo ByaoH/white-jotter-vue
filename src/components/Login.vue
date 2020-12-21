0a0c79225a0bf81dac34026351e42fc5564f3608
L7-BAG <ll789@gmail.com>
2020-12-21 21:45:19 +0800

    美化登陆界面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-      用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-      <br>
-      密码:<input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-      <br>
-      <button @click="login">登录</button>
-  </div>
+  <body id="paper">
+  <el-form
+    :model="loginForm"
+    :rules="rules"
+    class="login-container"
+    label-position="left"
+    ref="loginForm"
+    label-width="0px"
+    v-loading="loading"
+  >
+    <h3 class="login-title">系统登陆</h3>
+    <el-form-item prop="username">
+      <el-input
+        type="text"
+        v-model="loginForm.username"
+        placeholder="账号"
+      ></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input
+        type="password"
+        v-model="loginForm.password"
+        placeholder="密码"
+      ></el-input>
+    </el-form-item>
+    <el-checkbox
+      class="login_remember"
+      v-model="loginForm.checked"
+      label-position="left"
+    ><span style="color: #505458">记住密码</span></el-checkbox
+    >
+    <el-form-item style="width: 100%">
+      <el-button
+        type="primary"
+        style="width: 40%; background: #505458; border: none"
+        v-on:click="login"
+      >登录
+      </el-button
+      >
+      <router-link to="register"
+      >
+        <el-button
+          type="primary"
+          style="width: 40%; background: #505458; border: none;position: absolute;right: 0px"
+        >注册
+        </el-button
+        >
+      </router-link
+      >
+    </el-form-item>
+  </el-form>
+  </body>
 </template>
 
 <script>
@@ -14,31 +59,79 @@ export default {
   data () {
     return {
       loginForm: {
-        username: 'admin',
-        password: '123456'
+        username: '',
+        password: '',
+        checked: false
       },
-      responseResult: []
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6到 15 个字符', trigger: 'blur' }
+        ]
+      },
+      loading: false
     }
   },
   methods: {
     login () {
-      this.$axios.post('/login', this.loginForm)
-        .then(resp => {
-          if (resp.data.code === 200) {
-            console.log(resp.data)
-            this.$router.replace({ path: '/index' })
-            this.$message.success(resp.data.message)
-          } else {
-            this.$message.error(resp.data.message)
-          }
-        })
-        .catch(e => {
-        })
+      this.$refs.loginForm.validate(bool => {
+        if (bool) {
+          this.$axios.post('/login', this.loginForm)
+            .then(resp => {
+              if (resp.data.code === 200) {
+                console.log(resp.data)
+                this.$router.replace({ path: '/index' })
+                this.$message.success(resp.data.message)
+              } else {
+                this.$message.error(resp.data.message)
+              }
+            })
+            .catch(e => {
+            })
+        } else {
+          this.$message.warning('请正确输入')
+        }
+      })
     }
+  },
+  created () {
+    console.log(this.$route.query.redirect)
   }
 }
 </script>
 
 <style scoped>
+#paper {
+  background: url("../assets/img/bg/loginbg.jpg") no-repeat center;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+}
 
+.login-container {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 90px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px black;
+}
+
+.login-title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+
+.login_remember {
+  margin: 0px 0px 35px 0px;
+  text-align: left;
+}
 </style>
